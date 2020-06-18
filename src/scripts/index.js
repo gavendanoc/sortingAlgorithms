32e156b2e355768589c7485a3567638d7c61925f
gabriel <gabriel19oct@hotmail.com>
2020-06-17 20:56:10 -0500

    :recycle: function that draws array

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,54 @@
+/*
+    General Functions
+*/
 const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
 
+const size = 20;
+
+const updateItemHeight = () => {
+  const divfewUniqueArrayArea = document.getElementsByClassName("few-unique-array")[0];
+  const height = divfewUniqueArrayArea.getBoundingClientRect().height;
+  const root = document.documentElement;
+  const itemHeight = Math.floor(0.5 * (height / size));
+  root.style.setProperty("--item-height", `${itemHeight}px`);
+}
+
+updateItemHeight();
+
+
+window.addEventListener('resize', () => {
+  updateItemHeight();
+});
+
+
+
+const drawArray = (array, arrayClass) => {
+  // const divfewUniqueArrayArea = document.getElementsByClassName("few-unique-array")[0];
+  const divfewUniqueArrayArea = document.getElementsByClassName(arrayClass)[0];
+
+  const min = Math.min(...array);
+  const max = Math.max(...array);
+
+  const arrayItems = []
+
+  for (let i = 0; i < size; i++){
+    const item = document.createElement("div");
+    const width = map(array[i], min, max, 10, 90);
+
+    item.className += "item"
+
+    item.style.width = `${width}%`;
+    item.style.order = i;
+
+    divfewUniqueArrayArea.appendChild(item);
+    arrayItems.push(item);
+  }
+
+  return arrayItems;
+}
+
+
 // create few_unique_array
-const size = 50;
 const uniqueValues = 4;
 const fewUniqueArray = [];
 let initialValue = 0;
@@ -13,38 +60,19 @@ for (let i = 0; i < size; i++){
   fewUniqueArray.push(initialValue);
 }
 
+// draw in page
+drawArray(fewUniqueArray, "few-unique-array");
 
-console.log(fewUniqueArray);
 
+// create reversed arrays
+const reversedArray = [];
+
+for (let i = size; i > 0; i--){
+  reversedArray.push(i);
+}
 
 // draw in page
-const divfewUniqueArrayArea = document.getElementsByClassName("few-unique-array")[0];
-
-const min = Math.min(...fewUniqueArray);
-const max = Math.max(...fewUniqueArray);
-
-const updateItemHeight = () => {
-  const height = divfewUniqueArrayArea.getBoundingClientRect().height;
-  const root = document.documentElement;
-  const itemHeight = Math.floor(0.5 * (height / size));
-  root.style.setProperty("--item-height", `${itemHeight}px`);
-}
-
-updateItemHeight();
-
-for (let i = 0; i < size; i++){
-  const item = document.createElement("div");
-  const width = map(fewUniqueArray[i], min, max, 10, 90);
-
-  item.className += "item"
-
-  item.style.width = `${width}%`;
-  item.style.order = i;
-
-  divfewUniqueArrayArea.appendChild(item);
-}
+drawArray(reversedArray, "reversed-array");
 
 
-window.addEventListener('resize', () => {
-  updateItemHeight();
-});
+console.log(reversedArray);
